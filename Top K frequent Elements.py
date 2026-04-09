c0604493dad18607e74009fd680d8739844a7faa
Alex-fiqe <elooeliyas955@gmail.com>
2026-04-09 11:28:00 -0700

    Update Top K frequent Elements.py

--- Top K frequent Elements.py
+++ Top K frequent Elements.py
@@ -5,3 +5,6 @@ class Solution:
             fre[i]=fre.get(i,0) + 1
         sorted_nums=sorted(fre,key=fre.get,reverse=True)  
         return sorted_nums[:k]
+#sorted(freq)-->This means: sort the keys of the dictionary
+#key=fre.get-->this means: sort in their fre not in their key
+#reverse=True--> This means: reverse in decending order(the highest fre in the first)
